07148a86af15bb6c0481b7787fd76467d505f481
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-11-16 14:56:08 +0800

    Add autotest

---
+++ test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+_py="python"
+
+exclude_dirs=("__pycache__" "venv")
+
+failed=0
+
+# Loop through each directory
+for dir in */; do
+    # Run doctest on the Python file in the directory
+    dir_name="${dir::-1}"
+    if [[ " ${exclude_dirs[@]} " =~ " ${dir_name} " ]]; then
+        continue
+    fi
+    file_name="${dir_name}.py"
+    if [ ! -f "${dir_name}/${file_name}" ]; then
+        continue
+    fi
+    echo "Running doctest on ${dir_name}/${file_name}"
+    # Update the failed count
+    $_py -m doctest "${dir_name}/${file_name}"
+    if [ $? -ne 0 ]; then
+        failed=$((failed+1))
+    fi
+done
+
+# Fail if any tests failed
+if [ $failed -ne 0 ]; then
+    echo "Failed $failed tests"
+    exit 1
+fi
