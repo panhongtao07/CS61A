b13b8250f2f772405cfaa5199333e5febeb2030b
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-11-16 15:25:54 +0800

    HW2: lambda expression

--- hw02/hw02.py
+++ hw02/hw02.py
@@ -149,7 +149,7 @@ def mul_by_num(num):
     >>> y(-4)
     -8
     """
-    return ______
+    return lambda x: x * num
 
 
 def add_results(f1, f2):
@@ -172,7 +172,7 @@ def add_results(f1, f2):
     >>> a3(4)
     44
     """
-    return ______
+    return lambda x: f1(x) + f2(x)
 
 
 def mod_maker():
@@ -190,7 +190,7 @@ def mod_maker():
     >>> check(HW_SOURCE_FILE, 'mod_maker', ['If', 'IfExp']) # no if / if-else statements
     True
     """
-    return ______
+    return lambda x, y: x % y == 0 or x % y
 
 
 def lambda_math_syntax_check():
