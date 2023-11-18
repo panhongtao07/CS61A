21e849ea8447c9a6caf2631dee77aa3fa8483ec6
panhongtao <59553164+panhongtao07@users.noreply.github.com>
2023-11-18 17:56:45 +0800

    HW3: Hanoi

--- hw03/hw03.py
+++ hw03/hw03.py
@@ -176,6 +176,11 @@ def move_stack(n, start, end):
     """
     assert 1 <= start <= 3 and 1 <= end <= 3 and start != end, "Bad start/end"
     "*** YOUR CODE HERE ***"
+    if n == 0:
+        return
+    move_stack(n - 1, start, 6 - start - end)
+    print_move(start, end)
+    move_stack(n - 1, 6 - start - end, end)
 
 
 from operator import sub, mul
